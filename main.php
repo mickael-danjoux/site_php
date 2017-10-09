d62d61bd9712f57da0022041fabbbb67e546c974
BREVET MAEL <mael.brevet@gmail.com>
2017-10-09 10:53:37 +0200

    ajout de la connexion

--- main.php
+++ main.php
@@ -1,10 +1,36 @@
+<?php 
+	session_start();
+?>
+
 <!DOCTYPE html>
 <html>
+	<meta charset="utf-8">
+
 <head>
-	<title>	page</title>
+	<title>Site test</title>
+	<link rel="stylesheet" type="text/css" href="theme.css">
 </head>
 <body>
-bonjour, connard
-
+	<div class="wrapper">
+	<?php
+		if(isset($_GET['message'])){
+			echo $_GET['message'];
+		}
+	?>
+	<h1>Connexion</h1>
+	<form name="connexion" action="params.php" method="post">
+		<input type="text" name="id" placeholder="Login" required
+			<?php if(!empty($_SESSION['id'])){
+				echo 'value="'.$_SESSION['id'].'"';
+			} ?>> <br>
+		<input type="password" name="mdp" placeholder="Mot de passe" required
+			<?php if(!empty($_SESSION['mdp'])){
+				echo 'value="'.$_SESSION['mdp'].'"';
+			} ?>> <br>
+		<input type="submit">
+		<input type="reset"><br>
+	</form>
+	<a href="inscription.php">Inscription</a>
+	</div>
 </body>
 </html>
---
+++ params.php
@@ -0,0 +1,39 @@
+<?php
+	require "bdd.php";
+
+	session_start();
+	$_SESSION = $_POST;
+
+	//vérification si les champs sont remplis
+	if(empty($_POST['id']) || empty($_POST['mdp'])){
+		header("Location: main.php?message=Login ou mot de passe vide");
+	}
+	else{
+
+		$requete = "SELECT password FROM inscrit WHERE identifiant LIKE '" . $_POST['id'] . "'";
+		$stmt = $pdo->query($requete);
+		$res = $stmt->fetch();
+		
+
+		if(empty($res)){
+			header("Location: main.php?message=Mauvais identifiant");
+		}
+		else{
+			$exist = false;
+			foreach ($res as $inscrit) {
+				if($inscrit == $_POST['mdp'] ){
+					$exist = true;
+					break;
+				}
+			}
+			if(!$exist){
+				header("Location: main.php?message=Mauvais mot de passe");
+			}
+		}
+		
+
+		echo '<h1> connecté </h1>';
+
+	}
+
+?>
