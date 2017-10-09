c002a81985863e087f5e67f36a8ddb6d427b7b35
DANJOUX MICKAEL p1602272 <mickael.danjoux@etu.univ-lyon1.fr>
2017-10-09 12:25:42 +0200

    maj insciption

--- inscription.php
+++ inscription.php
@@ -15,7 +15,11 @@
 	<div class="wrapper">
 	<h1>Inscription</h1>
 	<?php
-		if(!empty($_POST['id']) && !empty($_POST['mdp'])){
+
+	//incription utilisateur
+	if (!empty($_POST)){
+		// on test les champs
+		if(!empty($_POST['id']) && !empty($_POST['mdp'])&&$_POST['confirm_password']==$_POST['mdp']){
 			$requete = "INSERT INTO utilisateur (login,password,admin) values (?,?,?)";
 			//echo $requete; die();
 			
@@ -24,11 +28,22 @@
 			$stmt->execute(array($_POST['id'],$_POST['mdp'],0));
 			header("Location: main.php?message=Bienvenue ".$_POST['id']);
 		}
-	?>
+		// on affiche un message d'erreur si les champs sont incorrect
+		elseif ($_POST['confirm_password']!=$_POST['mdp']) 
+		{ 
+			?>  <script language='javascript'>
+      			alert('Le mot de passe confirmé n\'est pas identique');
+   			 </script>
+  		 <?php
+		}
+	}
+		?>
+	
 	<form name="Inscription" method="post">
 		<input type="login" name="id" placeholder="Login"><br>
 			
 		<input type="password" name="mdp" placeholder="Mot de passe"><br>
+		<input type="password" name="confirm_password" placeholder="Confirmer mot de passe">
 			
 		<input type="submit">
 		<input type="reset"><br>
