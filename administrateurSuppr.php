bce46ab167a66d21c5234962f4bef117e8671110
BREVET MAEL <mael.brevet@gmail.com>
2017-10-26 12:25:03 +0200

    ajout bord bas du bandeau, ainsi que modif de la page suppression de photo

--- administrateurSuppr.php
+++ administrateurSuppr.php
@@ -9,29 +9,29 @@
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-	<title></title>
-	<link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-	<div id="bandeau">
-		<?php
-			//On regarde si on a un message d'erreur
-			if(isset($_GET['message'])){
-				echo $_GET['message'];
-			}
-		?>
-			<ul id="menu_admin">
-				<li><a href="administrateurAjout.php">Ajout photo</a></li>
-				<li><a href="administrateurSuppr.php">Supprimer photo</a></li>
-			</ul>
+	<head>
+		<title></title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<div id="bandeau">
+			<?php
+				//On regarde si on a un message d'erreur
+				if(isset($_GET['message'])){
+					echo $_GET['message'];
+				}
+			?>
+				<ul id="menu_admin">
+					<li><a href="administrateurAjout.php">Ajout photo</a></li>
+					<li><a href="administrateurSuppr.php">Supprimer photo</a></li>
+				</ul>
 
-		<?php	
-			//Formulaire de déconnexion
-				$form_deconnexion = new form("deconnexion","deconnexion.php","post");
-				$form_deconnexion->setsubmit("validerdeconnexion","Deconnexion");
-				$form_deconnexion->getform();
-		?>
-	</div>
-</body>
+			<?php	
+				//Formulaire de déconnexion
+					$form_deconnexion = new form("deconnexion","deconnexion.php","post");
+					$form_deconnexion->setsubmit("validerdeconnexion","Deconnexion");
+					$form_deconnexion->getform();
+			?>
+		</div>
+	</body>
 </html>
